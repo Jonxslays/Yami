62fbcbc342b1843670c36bf86b1cb4ffee415bd2
Jonx <jon@jonxtech.com>
2021-07-03 17:57:20 -0700

    update license in pyproject.toml

--- tests/test_yami.py
+++ tests/test_yami.py
@@ -1,20 +1,14 @@
-import pytest # type: ignore
+import pytest  # type: ignore
 
 from yami import Bot, __version__
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.2"
+    assert __version__ == "0.1.3"
 
 
 def test_bot() -> None:
-    bot = Bot(
-        token="1234",
-        prefix="x",
-        case_insensitive=False,
-        shun_bots=False,
-        owners=[123, 654]
-    )
+    bot = Bot(token="1234", prefix="x", case_insensitive=False, shun_bots=False, owners=[123, 654])
     assert bot.prefix == "x"
     assert bot._token == "1234"
     assert bot.case_insensitive == False
