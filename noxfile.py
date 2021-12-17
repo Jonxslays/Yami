6ea96b60c62d1adc9f29b852be80ac723c092a0e
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-12-17 07:53:54 -0700

    Add license to all source files

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,22 @@
+# Yami - A command handler that complements Hikari.
+# Copyright (C) 2021 Jonxslays
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
+
+from pathlib import Path
 
 import nox
 import toml
@@ -48,3 +66,31 @@ def formatting(session: nox.Session) -> None:
     session.run("isort", "yami", "tests", "-cq")
     session.run("black", ".", "--check")
     session.run("len8")
+
+
+@nox.session(reuse_venv=True)
+def licensing(session: nox.Session) -> None:
+    missing: list[Path] = []
+    files: list[Path] = [
+        *Path("./yami").rglob("*.py"),
+        *Path("./tests").glob("*.py"),
+        *Path(".").glob("*.py"),
+    ]
+
+    for path in files:
+        with open(path) as f:
+            for i, line in enumerate(f):
+                if i == 0:
+                    continue
+
+                if i > 1:
+                    break
+
+                if not line.startswith("# Copyright (C)"):
+                    missing.append(path)
+
+    if missing:
+        session.error(
+            "\nThe following files are missing their license:\n"
+            + "\n".join(f" - {m}" for m in missing)
+        )
